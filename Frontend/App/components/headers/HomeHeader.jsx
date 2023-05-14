01b01e384654556dec44762a473c2363cdf3af1e
janithGamage <janith.g@informaticsint.com>
2023-05-14 18:24:35 +0530

    fix: update Desc : update project

--- Frontend/App/components/headers/HomeHeader.jsx
+++ Frontend/App/components/headers/HomeHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect  } from 'react';
+import { useState, useEffect } from 'react';
 
 import {
     StyleSheet,
@@ -13,13 +13,13 @@ import AccountButton from '../buttons/AccountButton';
 import AccountPopup from '../popups/AccountPopup';
 
 
-const HomeHeader =( props )=> {
+const HomeHeader = (props) => {
 
-    const[accountPopup, setAccountPopup] = useState(false)
+    const [accountPopup, setAccountPopup] = useState(false)
 
     useEffect(() => {
         const handleBackButton = () => {
-            if(accountPopup) {
+            if (accountPopup) {
                 setAccountPopup(false)
                 return true;
             }
@@ -28,60 +28,60 @@ const HomeHeader =( props )=> {
                 return false;
             }
         };
-        
+
         BackHandler.addEventListener('hardwareBackPress', handleBackButton);
-        
+
         return () => {
-          BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+            BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
         };
     }, [accountPopup]);
-    
+
     return (
         <View style={styles.body}>
             <View style={styles.brand}>
-                <Image style={styles.logo} source={require('../../Assets/Logo/LogoSmall.png')}/>
+                <Image style={styles.logo} source={require('../../Assets/Logo/LogoSmall.png')} />
                 <Text style={styles.text}>{props.Title}</Text>
             </View>
 
             <View style={styles.account}>
-                <AccountButton press_Action={()=>setAccountPopup(true)}></AccountButton>
+                <AccountButton press_Action={() => setAccountPopup(true)}></AccountButton>
             </View>
 
-            {accountPopup && (<AccountPopup logout_User={props.logout_Action}/>)}
+            {accountPopup && (<AccountPopup logout_User={props.logout_Action} />)}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    body : {
+    body: {
         flexDirection: 'row',
-      backgroundColor : '#005F41',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      zIndex:1
+        backgroundColor: '#005F41',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        zIndex: 1
     },
 
-    text : {
-      marginVertical:9,
-      color: 'white',
-      fontSize: 32,
-      fontWeight: 600,
+    text: {
+        marginVertical: 9,
+        color: 'white',
+        fontSize: 32,
+        fontWeight: 600,
     },
 
     logo: {
         height: 40,
-        width:40,
-        marginRight:6
+        width: 40,
+        marginRight: 6
     },
-    
-    brand : {
+
+    brand: {
         flexDirection: 'row',
         alignItems: 'center',
-        marginLeft:10
+        marginLeft: 10
     },
 
     account: {
-        marginRight:15
+        marginRight: 15
     },
 })
 

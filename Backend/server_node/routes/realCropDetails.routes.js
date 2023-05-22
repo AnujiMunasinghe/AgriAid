e3bcacff62d58272f7fc4ca93484cf89f17b2413
janithGamage <janith.g@informaticsint.com>
2023-05-22 21:51:38 +0530

    fix: update desc : update project

--- Backend/server_node/routes/realCropDetails.routes.js
+++ Backend/server_node/routes/realCropDetails.routes.js
@@ -5,39 +5,39 @@ const RealCropDetails = require("../models/RealCropDetails.model"); // Assuming 
 
 // GET API endpoint to retrieve all data
 router.get("/real-data/all", async (req, res) => {
-    try {
-      // Find all data
-      const data = await RealCropDetails.find();
-  
-      if (!data || data.length === 0) {
-        return res.status(404).json({ error: "No data found" });
-      }
-  
-      res.json(data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  });
+  try {
+    // Find all data
+    const data = await RealCropDetails.find();
 
-  
+    if (!data || data.length === 0) {
+      return res.status(404).json({ error: "No data found" });
+    }
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
 // GET API endpoint to retrieve data based on region and crop
 router.get("/real-data", async (req, res) => {
-    const { region, crop } = req.query;
+  const { region, crop } = req.query;
 
-    try {
-        // Find data based on region and crop
-        const data = await RealCropDetails.findOne({ Region: region, Crop: crop });
+  try {
+    // Find data based on region and crop
+    const data = await RealCropDetails.findOne({ Region: region, Crop: crop });
 
-        if (!data) {
-            return res.status(404).json({ error: "Data not found" });
-        }
-
-        res.json(data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
+    if (!data) {
+      return res.status(404).json({ error: "Data not found" });
     }
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
